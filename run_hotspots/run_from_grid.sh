2ae9a6167ccf47bb08c456560616e7cc25a9a387
prcurran <pc564@cam.ac.uk>
2019-01-29 14:39:47 +0000

    updated runner

--- run_hotspots/run_from_grid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {1..5};
-do 
-	python "/home/pcurran/phd/phd-scripts/run_hotspots/run_from_grid.py" "/home/pcurran/fragment-hotspot-results/patel_set/$i/out"
-	
-done
--- run_hotspots/run_from_protein.py
+++ run_hotspots/run_from_protein.py
@@ -1,19 +1,18 @@
-from fragment_hotspot_maps.fragment_hotspot_maps import Hotspots
-from ccdc.protein import Protein
-from ccdc import io
-from ccdc.io import MoleculeReader
-from ccdc.utilities import Grid
-import sys
-from ccdc.molecule import Coordinates
-from os.path import join, dirname, exists, abspath
-from os import mkdir
+from __future__ import print_function
+
 import argparse
+from os.path import dirname, abspath
 
-from ccdc.io import MoleculeReader, MoleculeWriter
+from ccdc.cavity import Cavity
+from ccdc.protein import Protein
+
+from hotspots.calculation import Runner
+from hotspots.hs_io import HotspotWriter
 
 
-class Runner(argparse.ArgumentParser):
+class Organiser(argparse.ArgumentParser):
     """ class to initiate a HotspotResults instance"""
+
     def __init__(self):
         super(self.__class__, self).__init__(description=__doc__)
         self.add_argument(
@@ -21,64 +20,73 @@ class Runner(argparse.ArgumentParser):
             help='pdb file path'
         )
         self.add_argument(
-            '-g', '--ghecom_executable',
-            default=None,
-            help='path to ghecom executable, if None, Ligsite will be used'
+            '-b', '--buriedness_method',
+            default='ghecom',
+            help='method used to calculate buriedness (default = "ghecom")'
         )
+
+        self.add_argument(
+            '-w', '--remove_waters',
+            default=True,
+            help='remove waters in prot preparation(default = True)'
+        )
+        
+        self.add_argument(
+            '-p', '--prepare',
+            default=True,
+            help='prepare_protein(default = True)'
+        )
+
+        self.add_argument(
+            '-z', '--zipped',
+            default=True,
+            help='compress output files(default = True)'
+        )
+
         self.args = self.parse_args()
 
         self.in_dir = dirname(abspath(self.args.prot_fname))
-        self.out_dir = join(self.in_dir, "out")
 
     def prepare_protein(self):
         """default protein preparation settings on the protein"""
         self.prot = Protein.from_file(self.args.prot_fname)
-        self.prot.remove_all_waters()
+        self.prot.add_hydrogens()
         for lig in self.prot.ligands:
             self.prot.remove_ligand(lig.identifier)
         self.prot.remove_all_metals()
+        if self.args.remove_waters:
+            self.prot.remove_all_waters()
 
-    def run(self, prepare=False):
+    def run(self, cavity=True):
         """from fragment hotspot calc from protein"""
-        h = Hotspots()
-        if prepare:
+        h = Runner()
+        settings = Runner.Settings(sphere_maps=False)
+        if self.args.prepare:
             self.prepare_protein()
         else:
             self.prot = Protein.from_file(self.args.prot_fname)
 
-        h.out_dir = self.out_dir
-        result = h.from_protein(prot=self.prot,
+        if cavity:
+            cavs = Cavity.from_pdb_file(self.args.prot_fname)
+            print(cavs)
+        else:
+            cavs = None
+
+        result = h.from_protein(protein=self.prot,
                                 charged_probes=True,
-                                ghecom_executable=self.args.ghecom_executable,
-                                output_directory=self.out_dir)
+                                buriedness_method=self.args.buriedness_method,
+                                cavities=cavs,
+                                nprocesses=5,
+                                settings=settings)
 
-        result.out_dir = self.out_dir
-        result.output_pymol_file()
-        for p, mols in result.sampled_probes.items():
-            if not exists(self.out_dir):
-                mkdir(self.out_dir)
-            with MoleculeWriter(join(self.out_dir, "{}_probes.mol2".format(p))) as w:
-                for mol in mols:
-                    w.write(mol)
+        with HotspotWriter(path=self.in_dir, zip_results=self.args.zipped) as writer:
+            writer.write(result)
 
-        objs = result.extract_hotspots(out_dir=dirname(self.out_dir), pharmacophores=True)
-        for i, hr in enumerate(objs):
-            out = join(dirname(self.out_dir), "hotspot_boundaries", "{}".format(i))
-
-            if not exists(join(dirname(self.out_dir), "hotspot_boundaries")):
-                mkdir((join(dirname(self.out_dir), "hotspot_boundaries")))
-
-            if not exists(out):
-                mkdir(out)
-            hr.out_dir = out
-            hr.output_pymol_file()
-            hr.pharmacophore.out_dir = out
-            hr.pharmacophore.write(join(out, "pharmacophore.json"))
 
 def main():
-    r = Runner()
+    r = Organiser()
     r.run()
+
 
 if __name__ == "__main__":
     main()
-
